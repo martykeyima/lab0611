aa3f930f516b0fff2e6df0f6655a7e4b7372801d
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-11-06 17:33:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lab</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <style>
+            p{
+                color: slategrey;
+            }
+
+            h5{
+                color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card-deck">
+                <div class="card">
+                  <img src="img/creative/creative7.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Sea healing</h5>
+                    <p class="card-text">เพราะทุกคนต้องการการเยียวยา.</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/staffs/staff3.jpg" alt="Avatar" height=25>
+                    <small class="text-muted">Active</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/banner/banner5.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Fuckup with Everthings</h5>
+                    <p class="card-text">บางครั้งเราก็ต้องปล่อยวาง.</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/life/life.jpg" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 0.01 secs ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="container">
+                  <div class="card-deck">
+                      <div class="card">
+                        <img src="img/work/work8.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">ทำงานบ้าง</h5>
+                          <p class="card-text">อย่าขี้เกียจไอคอม.</p>
+                        </div>
+                        <div class="card-footer">
+                          <img style="border-radius: 50%;" src="img/life/life9.jpg" alt="Avatar" height=25>
+                          <small class="text-muted">Active</small>
+                          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                            </svg>
+                        </div>
+                      </div>
+                <div class="card">
+                  <img src="img/work/work6.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Lonely Sometime</h5>
+                    <p class="card-text">ออกมาหาที่เงียบๆ พักความคิดสักแปป</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img/life/life5.jpg" alt="Avatar" height=25>
+                    <small class="text-muted">Disappear</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="blue" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+              </div>
+          </div>
+    </body>
+</html>
